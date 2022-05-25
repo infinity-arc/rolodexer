b80dbf6a73dd945f948077fccaaf5a1266228580
C1I7F05D-C53545 <cliff.crerar@gmail.com>
2022-05-26 00:43:48 +0200

    Minor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,43 @@
 import { environment } from './../environments/environment';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+	selector: 'app-root',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {
-	environment.production && this.createGTag();
-  }
+export class AppComponent implements OnInit {
+	constructor() {
 
+	}
 
-  createGTag() {
-	  const createScriptTag = () => document.createElement('script');
-	  const scriptTagOne = createScriptTag();
-	  const scriptTagTwo = createScriptTag();
+	ngOnInit(): void {
+		environment.production && this.createGTag();
+	}
 
-	  scriptTagOne.async = true;
-	  scriptTagOne.src = 'https://www.googletagmanager.com/gtag/js?id=G-VZX1DVY5QL';
-	  scriptTagTwo.innerText = `
+	createGTag() {
+		const createScriptTag = () => document.createElement('script');
+		const scriptTagOne = createScriptTag();
+		const scriptTagTwo = createScriptTag();
+
+		scriptTagOne.async = true;
+		scriptTagOne.src = 'https://www.googletagmanager.com/gtag/js?id=G-VZX1DVY5QL';
+		scriptTagTwo.innerText = `
 		window.dataLayer = window.dataLayer || []; const gtag = () => dataLayer.push(arguments);gtag('js', new Date());gtag('config', 'G-VZX1DVY5QL');
 	  `
-	  document.head.appendChild(scriptTagOne);
-	  document.head.appendChild(scriptTagTwo);
+		document.head.appendChild(scriptTagOne);
+		document.head.appendChild(scriptTagTwo);
 
-	  /*
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-VZX1DVY5QL"></script>
-	<script>
-	  window.dataLayer = window.dataLayer || [];
-	  function gtag(){dataLayer.push(arguments);}
-	  gtag('js', new Date());
+		/*
+	  <!-- Global site tag (gtag.js) - Google Analytics -->
+	  <script async src="https://www.googletagmanager.com/gtag/js?id=G-VZX1DVY5QL"></script>
+	  <script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
 
-	  gtag('config', 'G-VZX1DVY5QL');
-	</script>
-	*/
-  }
+		gtag('config', 'G-VZX1DVY5QL');
+	  </script>
+	  */
+	}
 }
